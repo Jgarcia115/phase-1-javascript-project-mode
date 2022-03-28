69d5d97e0e4f25a25b2cea0a47948ecbba3c2304
Jonathan <jono_95@yahoo.com>
2022-03-28 12:23:13 -0400

    fixed list and comment bugs

--- index.js
+++ index.js
@@ -25,39 +25,43 @@ document.addEventListener('DOMContentLoaded', () => {
 function handleComment(e) {
     e.preventDefault()
     let review = e.target.review.value
-    let p = document.createElement('p')
-    let card = document.querySelector("#drink")
-    p.innerText = review
-    card.appendChild(p)
+    let li = document.createElement('li')
+    let card = e.target.querySelector("#no-numbers")
+    li.innerText = review
+    card.appendChild(li)
 }
 
 //Rendering List
 function renderDrinks(drinks) {
     let drinksOl = document.querySelector('#drink-list')
+    drinksOl.innerHTML = ""
     drinks.forEach(element => {
         let li = document.createElement('li')
         li.id = "drink"
         li.textContent = element.strDrink
         drinksOl.appendChild(li)
         let img = document.createElement('img')
+        img.id = "pic"
         img.src = element.strDrinkThumb
         li.appendChild(img)
         let comment = document.createElement('form')
         let label = document.createElement('label')
         let input = document.createElement('input')
         let submit = document.createElement('input')
-        comment.id = "comment"
+        let reviews = document.createElement('ul')
+        reviews.id = "no-numbers"
+        comment.classList.add("comment")
         submit.type = "submit"
         submit.value = "Post"
         submit.id = "button"
         input.id = "review"
         input.type = "text"
         input.placeholder = "Type here!"
-        label.innerHTML = "Leave Review:  "
+        label.innerHTML = "Leave a Review:  "
         li.appendChild(comment).appendChild(label)
         comment.appendChild(input)
         comment.appendChild(submit)
-        document.querySelector('form').addEventListener("submit", handleComment)
-
+        comment.appendChild(reviews)
+        comment.addEventListener("submit", handleComment)
     })
 }
